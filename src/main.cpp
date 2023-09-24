3d96bf46da0643a232898d95e4efad08ff04a5b7
Talha Cetin <talhacetin96@hotmail.com>
2023-09-24 19:29:30 +0300

    - Added start button algorithm - Added Checksum algorithm - Revised TCPMessage_t struct - Added battery led algorithm - Added ACK response

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,18 @@
 #include <string.h>
 #include "pin_definitions.h"
 
-#define DEBUG_PRINT 1
+#define DEBUG_PRINT         1
+#define START_STOP_COUNTER  150 /* 150*20 ms => 3 second */
+#define BATERRY_FULL_VALUE  0xCE /* Approximately 8V on a 6V battery */
+#define BATERRY_LOW_VALUE   0x8C /* Approximately 5.4V on a 6V battery */
+#define ACK_MESSAGE         "ACK"
 
 void vTCPConnectionTask(void *pvParameters);
 void xIOReaderTask(void *pvParameters);
 void CalculateChecksum(uint8_t *dataPointer, uint8_t size);
 void SerialDebugPrint(int number);
 void SerialDebugPrint(const char *text);
+void SetTCPMessageDefault(void);
 
 const int test_pin = BATTERY_LED_RED;
 const int pins[PIN_COUNT] = {JOYSTICK_X, JOYSTICK_Y, DRIVE_SPEED, BRUSH_SPEED, BATTERY_VOLTAGE, WATER_BUTTON, LAMP_BUTTON, START_BUTTON, PASS_BUTTON, BRUSH_FRONT_CW, BRUSH_FRONT_CCW, BRUSH_REAR_CW, BRUSH_REAR_CCW, EMERGENCY_STOP_BUTTON, WARNING_LED_RED, WARNING_LED_GREEN, BATTERY_LED_RED, BATTERY_LED_GREEN};
@@ -21,7 +26,6 @@ TaskHandle_t xIOReaderTaskHandle = NULL;
 TaskHandle_t xTCPConnectionTaskHandle = NULL;
 
 SemaphoreHandle_t xSerialPrintSemaphore;
-SemaphoreHandle_t xIOControlSemaphore;
 SemaphoreHandle_t xTCPMessageUpdateSemaphore;
 QueueHandle_t xTCPMessageQueue;
 
@@ -43,6 +47,7 @@ const int TCPConnectTimeout = 1000;
 
 TimerHandle_t TCPMessageTimer;
 bool TCPMessageTimeout = false;
+bool remoteStarted = false;
 
 typedef struct
 {
@@ -53,7 +58,6 @@ typedef struct
 	uint8_t joystickY;
     uint8_t driveSpeed;
     uint8_t brushSpeed;
-    uint8_t batteryVoltage;
 	union
 	{
 		uint8_t buttons;
@@ -78,18 +82,6 @@ typedef struct
             uint8_t                 :7;
 		};
 	};
-    union
-	{
-		uint8_t leds;
-		struct
-		{
-			uint8_t warningLedRed   :1;
-            uint8_t warningLedGreen :1;
-            uint8_t batteryLedRed   :1;
-            uint8_t batteryLedGreen :1;
-            uint8_t                 :4;
-		};
-	};
 	uint8_t checksum;
 }TCPMessage_t;
 TCPMessage_t TCPMessage;
@@ -101,6 +93,10 @@ void TCPMessageTimerCallback(TimerHandle_t xTimer)
 
 void setup()
 {
+    Serial.begin(9600);
+    xSerialPrintSemaphore = xSemaphoreCreateMutex();
+    xTCPMessageUpdateSemaphore = xSemaphoreCreateMutex();
+    SerialDebugPrint("Serial Begin");
     for (int i = 0; i < PIN_COUNT; i++)
     {
         if (i < 5)
@@ -116,17 +112,14 @@ void setup()
             pinMode(pins[i], OUTPUT);
         }
     }
-
+    SerialDebugPrint("Pin mode set");
     TCPConnectionState = CONNECTING_TO_WIFI;
     digitalWrite(WARNING_LED_RED, HIGH);
     digitalWrite(WARNING_LED_GREEN, LOW);
-
-    Serial.begin(9600);
-
-    xSerialPrintSemaphore = xSemaphoreCreateMutex();
-    xIOControlSemaphore = xSemaphoreCreateMutex();
-    xTCPMessageUpdateSemaphore = xSemaphoreCreateMutex();
-
+    SerialDebugPrint("xSerialPrintSemaphore Created");
+    SerialDebugPrint("xTCPMessageUpdateSemaphore Created");
+    SetTCPMessageDefault();
+    SerialDebugPrint("TCP Message Default");
     TCPMessageTimer = xTimerCreate("TCPMessageTimer", pdMS_TO_TICKS(1000), pdTRUE, (void *)0, TCPMessageTimerCallback);
     if (TCPMessageTimer == NULL)
     {
@@ -136,7 +129,7 @@ void setup()
             /* Error! */
         }
     }
-
+    SerialDebugPrint("TCPMessageTimer Created");
     BaseType_t xReturned;
     xReturned = xTaskCreate(xIOReaderTask,
                            "xIOReaderTaskHandle",
@@ -180,211 +173,201 @@ void setup()
 
 void loop()
 {
-    /*
-    for (int i = 0; i < PIN_COUNT; i++)
-    {
-        if (i < 4)
-        {
-            pin_values[i] = analogRead(pins[i]);
-        }
-        else
-        {
-            pin_values[i] = digitalRead(pins[i]);
-        }
-    }
-
-    for (int i = 4; i < PIN_COUNT; i++)
-    {
-        if (old_pin_values[i] != pin_values[i])
-        {
-            Serial.println("Pin number : " + String(pins[i]) + " " + "Pin val : " + String(pin_values[i]));
-            old_pin_values[i] = pin_values[i];
-        }
-    }
-    Serial.println("Joystick X :  " + String(pin_values[0]));
-    Serial.println("Joystick Y :  " + String(pin_values[1]));
-    Serial.println("BRUSH SPEED :  " + String(pin_values[2]));
-    Serial.println("DRIVE SPEED :  " + String(pin_values[3]));
-    delay(1000);
-    */
+    /* Leave Empty! */
 }
 
 void xIOReaderTask(void *pvParameters)
 {
     configASSERT(((uint32_t)pvParameters) == 1);
+    TickType_t xLastWakeTime;
+    xLastWakeTime = xTaskGetTickCount();
+    uint8_t startButtonTimerCounter = 0;
+    uint8_t batteryVoltage = 0;
     TCPMessage_t localTCPMessage = {.headerT = 'T', .headerC = 'C', .headerP = 'P'};
     for (;;)
     {
-        /* Analog Inputs */
-        localTCPMessage.joystickX        = analogRead(JOYSTICK_X) / 16;
-        localTCPMessage.joystickY        = analogRead(JOYSTICK_Y) / 16;
-        localTCPMessage.driveSpeed       = analogRead(DRIVE_SPEED) / 16;
-        localTCPMessage.brushSpeed       = analogRead(BRUSH_SPEED) / 16;
-        localTCPMessage.batteryVoltage   = analogRead(BATTERY_VOLTAGE) / 16;
+        /* Read Analog Inputs */
+        localTCPMessage.joystickX       = analogRead(JOYSTICK_X) / 16;
+        localTCPMessage.joystickY       = analogRead(JOYSTICK_Y) / 16;
+        localTCPMessage.driveSpeed      = (4095 - analogRead(DRIVE_SPEED)) / 16; /* Because pins are connected reverse */
+        localTCPMessage.brushSpeed      = (4095 - analogRead(BRUSH_SPEED)) / 16; /* Because pins are connected reverse */
+        batteryVoltage                  = analogRead(BATTERY_VOLTAGE) / 16;
+        
+        /* Check the battery value */
+        if (batteryVoltage <= BATERRY_LOW_VALUE)
+        {
+            digitalWrite(BATTERY_LED_RED, HIGH);
+            digitalWrite(BATTERY_LED_GREEN, LOW);
+        }
+        else
+        {
+            digitalWrite(BATTERY_LED_RED, LOW);
+            digitalWrite(BATTERY_LED_GREEN, HIGH);
+        }
 
-        /* Digital Inputs */
-        localTCPMessage.waterButton      = digitalRead(WATER_BUTTON);
-        localTCPMessage.lampButton       = digitalRead(LAMP_BUTTON);
-        localTCPMessage.startButton      = digitalRead(START_BUTTON);
-        localTCPMessage.passButton       = digitalRead(PASS_BUTTON);
-        localTCPMessage.brushFrontCW     = digitalRead(BRUSH_FRONT_CW);
-        localTCPMessage.brushFrontCCW    = digitalRead(BRUSH_FRONT_CCW);
-        localTCPMessage.brushRearCW      = digitalRead(BRUSH_REAR_CW);
-        localTCPMessage.brushRearCCW     = digitalRead(BRUSH_REAR_CCW);
-        localTCPMessage.emergencyButton  = digitalRead(EMERGENCY_STOP_BUTTON);
+        /* Read Digital Inputs */
+        localTCPMessage.waterButton     = digitalRead(WATER_BUTTON);
+        localTCPMessage.lampButton      = digitalRead(LAMP_BUTTON);
+        localTCPMessage.passButton      = digitalRead(PASS_BUTTON);
+        localTCPMessage.brushFrontCW    = digitalRead(BRUSH_FRONT_CW);
+        localTCPMessage.brushFrontCCW   = digitalRead(BRUSH_FRONT_CCW);
+        localTCPMessage.brushRearCW     = digitalRead(BRUSH_REAR_CW);
+        localTCPMessage.brushRearCCW    = digitalRead(BRUSH_REAR_CCW);
+        localTCPMessage.emergencyButton = digitalRead(EMERGENCY_STOP_BUTTON);
+        localTCPMessage.startButton     = digitalRead(START_BUTTON);
 
-        /* Digital Outputs */
-        xSemaphoreTake(xIOControlSemaphore, portMAX_DELAY);   
-        localTCPMessage.warningLedRed    = digitalRead(WARNING_LED_RED);
-        localTCPMessage.warningLedGreen  = digitalRead(WARNING_LED_GREEN);
-        localTCPMessage.batteryLedRed    = digitalRead(BATTERY_LED_RED);
-        localTCPMessage.batteryLedGreen  = digitalRead(BATTERY_LED_GREEN);
-        xSemaphoreGive(xIOControlSemaphore);
+        /* Check the start button for remote start/stop */
+        if (localTCPMessage.startButton == HIGH)
+        {
+            startButtonTimerCounter++;
+            if (startButtonTimerCounter == START_STOP_COUNTER)
+            {
+                startButtonTimerCounter = 0;
+                remoteStarted = !remoteStarted;
+            }
+        }
+        else
+        {
+            startButtonTimerCounter = 0;
+        }
 
-        /* Updating TCPMessage */
+        /* Update TCPMessage */
         xSemaphoreTake(xTCPMessageUpdateSemaphore, portMAX_DELAY);   
         memcpy((uint8_t*)&TCPMessage, (uint8_t*)&localTCPMessage, sizeof(TCPMessage_t));
         xSemaphoreGive(xTCPMessageUpdateSemaphore);
 
-        taskYIELD();
+        /* Repeat the task after 20 ms */
+        vTaskDelayUntil(&xLastWakeTime, (20/portTICK_RATE_MS));
     }
 }
 
 void vTCPConnectionTask(void *pvParameters)
 {
     configASSERT(((uint32_t)pvParameters) == 1);
-
-    /* 
-        Assigning static IP because TP-Link AP does not have DHCP
-        IPAddress localIP(192, 168, 0, 201);
-        IPAddress subnet(255, 255, 0, 0);
-
-        /* Subnet and IP is enough
-        IPAddress primaryDNS(0, 0, 0, 0);
-        IPAddress secondaryDNS(0, 0, 0, 0);
-        IPAddress gateway(0, 0, 0, 0);
-
-        if (!WiFi.config(localIP, gateway, subnet, primaryDNS, secondaryDNS))
-        {
-            SerialDebugPrint("Failed to IP and subnet");
-            while (1)
-            {
-                vTaskDelay(10000);
-            }
-        }
-    */
-
     for (;;)
     {
-        switch (TCPConnectionState)
+        if (remoteStarted == true)
         {
-            case CONNECTING_TO_WIFI:
+            switch (TCPConnectionState)
             {
-                wl_status_t wifiBeginStatus = WiFi.begin(ssid, password);
-                while (WiFi.status() != WL_CONNECTED)
+                case CONNECTING_TO_WIFI:
                 {
-                    delay(1000);
-                    SerialDebugPrint("Connecting to WiFi..");
-                }
-                SerialDebugPrint("Wifi Connected! SSID: ");
-                SerialDebugPrint(ssid);
-                SerialDebugPrint(" - Password: ");
-                SerialDebugPrint(password);
-                TCPConnectionState = CONNECTING_TO_TCP_SERVER;
-                break;
-            }
-            case CONNECTING_TO_TCP_SERVER:
-            {
-                if (client.connect(ip, port, TCPConnectTimeout) == 0)
-                {
-                    SerialDebugPrint("Client Connect Fail!");
-                    SerialDebugPrint("Error No: ");
-                    SerialDebugPrint(errno);
-                    SerialDebugPrint(" - Error Text: ");
-                    SerialDebugPrint(strerror(errno));
-                    if (WiFi.status() != WL_CONNECTED)
+                    wl_status_t wifiBeginStatus = WiFi.begin(ssid, password);
+                    while (WiFi.status() != WL_CONNECTED)
                     {
-                        WiFi.disconnect();
+                        vTaskDelay(1000 / portTICK_PERIOD_MS);
+                        SerialDebugPrint("Connecting to WiFi..");
+                    }
+                    SerialDebugPrint("Wifi Connected! SSID: ");
+                    SerialDebugPrint(ssid);
+                    SerialDebugPrint(" - Password: ");
+                    SerialDebugPrint(password);
+                    TCPConnectionState = CONNECTING_TO_TCP_SERVER;
+                    break;
+                }
+                case CONNECTING_TO_TCP_SERVER:
+                {
+                    if (client.connect(ip, port, TCPConnectTimeout) == 0)
+                    {
+                        SerialDebugPrint("Client Connect Fail!");
+                        SerialDebugPrint("Error No: ");
+                        SerialDebugPrint(errno);
+                        SerialDebugPrint(" - Error Text: ");
+                        SerialDebugPrint(strerror(errno));
+                        if (WiFi.status() != WL_CONNECTED)
+                        {
+                            WiFi.disconnect();
+                            close(client.fd());
+                            client.stop();
+                            TCPConnectionState = CONNECTING_TO_WIFI;
+                        }
+                    }
+                    else
+                    {
+                        SerialDebugPrint("Client Connected! IP: ");
+                        SerialDebugPrint(ip);
+                        SerialDebugPrint(" - Port: ");
+                        SerialDebugPrint(port);
+                        TCPConnectionState = CONNECTION_ESTABLISHED;
+                        digitalWrite(WARNING_LED_GREEN, HIGH);
+                        digitalWrite(WARNING_LED_RED, LOW);
+                    }
+                    break;
+                }
+                case CONNECTION_ESTABLISHED:
+                {
+                    if (xTimerReset(TCPMessageTimer, 10) != pdPASS) 
+                    {
+                        SerialDebugPrint("Failed to start timer!");
+                    }
+                    while (client.connected())
+                    {
+                        if (TCPMessageTimeout)
+                        {
+                            TCPMessageTimeout = false;
+                            break;
+                        }
+
+                        if (remoteStarted == false)
+                        {
+                            SetTCPMessageDefault();
+                            xSemaphoreTake(xTCPMessageUpdateSemaphore, portMAX_DELAY);   
+                            client.write((char *)&TCPMessage, sizeof(TCPMessage_t));
+                            xSemaphoreGive(xTCPMessageUpdateSemaphore);
+                            break;
+                        }
+
+                        int tcpBufferCount = client.available();
+                        if (tcpBufferCount > 2)
+                        {
+                            uint8_t message[3] = {0};
+                            client.readBytes(message, 3);
+                            if (strncmp((const char*)message, ACK_MESSAGE, 3) == 0)
+                            {
+                                if (xTimerReset(TCPMessageTimer, 10) != pdPASS) 
+                                {
+                                    SerialDebugPrint("Failed to reset timer!");
+                                }
+                            }
+                            else
+                            {
+                                SerialDebugPrint("Not ACK!!");
+                            }
+                        }
+                        CalculateChecksum((uint8_t*)&TCPMessage, sizeof(TCPMessage_t));
+                        xSemaphoreTake(xTCPMessageUpdateSemaphore, portMAX_DELAY);   
+                        client.write((char *)&TCPMessage, sizeof(TCPMessage_t));
+                        xSemaphoreGive(xTCPMessageUpdateSemaphore);
+                        vTaskDelay(100 / portTICK_PERIOD_MS);
+                    }
+
+                    if ((WiFi.status() != WL_CONNECTED) || (remoteStarted == false))
+                    {
                         close(client.fd());
                         client.stop();
+                        WiFi.disconnect();
                         TCPConnectionState = CONNECTING_TO_WIFI;
                     }
-                }
-                else
-                {
-                    SerialDebugPrint("Client Connected! IP: ");
-                    SerialDebugPrint(ip);
-                    SerialDebugPrint(" - Port: ");
-                    SerialDebugPrint(port);
-                    TCPConnectionState = CONNECTION_ESTABLISHED;
-                    xSemaphoreTake(xIOControlSemaphore, portMAX_DELAY);   
-                    digitalWrite(WARNING_LED_GREEN, HIGH);
-                    digitalWrite(WARNING_LED_RED, LOW);
-                    xSemaphoreGive(xIOControlSemaphore);
-                }
-                break;
-            }
-            case CONNECTION_ESTABLISHED:
-            {
-                if (xTimerReset(TCPMessageTimer, 10) != pdPASS) 
-                {
-                    SerialDebugPrint("Failed to start timer!");
-                }
-                while (client.connected())
-                {
-                    if (TCPMessageTimeout)
+                    else
                     {
-                        TCPMessageTimeout = false;
-                        break;
+                        close(client.fd());
+                        client.stop();
+                        TCPConnectionState = CONNECTING_TO_TCP_SERVER;
                     }
-
-                    int tcpBufferCount = client.available();
-                    if (tcpBufferCount > 0)
+                    
+                    if (xTimerStop(TCPMessageTimer, 10) != pdPASS) 
                     {
-                        if (xTimerReset(TCPMessageTimer, 10) != pdPASS) 
-                        {
-                            SerialDebugPrint("Failed to reset timer!");
-                        }
-                        for (int i = 0; i < tcpBufferCount; i++)
-                        {
-                            int discardData = client.read();
-                        }
+                        SerialDebugPrint("Failed to stop timer!");
                     }
-
-                    xSemaphoreTake(xTCPMessageUpdateSemaphore, portMAX_DELAY);   
-                    client.write((char *)&TCPMessage, sizeof(TCPMessage_t));
-                    xSemaphoreGive(xTCPMessageUpdateSemaphore);
-                    delay(100);
+                    SerialDebugPrint("Reconnection...");
+                    digitalWrite(WARNING_LED_GREEN, LOW);
+                    digitalWrite(WARNING_LED_RED, HIGH);
+                    break;
                 }
-
-                if (WiFi.status() != WL_CONNECTED)
+                default:
                 {
-                    WiFi.disconnect();
-                    close(client.fd());
-                    client.stop();
-                    TCPConnectionState = CONNECTING_TO_WIFI;
+                    break;
                 }
-                else
-                {
-                    close(client.fd());
-                    client.stop();
-                    TCPConnectionState = CONNECTING_TO_TCP_SERVER;
-                }
-                
-                if (xTimerStop(TCPMessageTimer, 10) != pdPASS) 
-                {
-                    SerialDebugPrint("Failed to stop timer!");
-                }
-                SerialDebugPrint("Reconnection...");
-                xSemaphoreTake(xIOControlSemaphore, portMAX_DELAY);   
-                digitalWrite(WARNING_LED_GREEN, LOW);
-                digitalWrite(WARNING_LED_RED, HIGH);
-                xSemaphoreGive(xIOControlSemaphore);
-                break;
-            }
-            default:
-            {
-                break;
             }
         }
         taskYIELD();
@@ -399,7 +382,27 @@ void CalculateChecksum(uint8_t *dataPointer, uint8_t size)
 		checksum ^= dataPointer[i];
 	}
 	checksum ^= 255;
-    dataPointer[size - 1] == checksum;
+    dataPointer[size - 1] = checksum;
+}
+
+void SetTCPMessageDefault()
+{
+    TCPMessage_t localTCPMessage = 
+    {
+        .headerT    = 'T', 
+        .headerC    = 'C', 
+        .headerP    = 'P',
+        .joystickX  = 128,
+        .joystickY  = 128,
+        .driveSpeed = 0,
+        .brushSpeed = 0,
+        .buttons    = 0,
+        .data       = 0,
+        .checksum   = 184
+    };
+    xSemaphoreTake(xTCPMessageUpdateSemaphore, portMAX_DELAY);   
+    memcpy((uint8_t*)&TCPMessage, (uint8_t*)&localTCPMessage, sizeof(TCPMessage_t));
+    xSemaphoreGive(xTCPMessageUpdateSemaphore);
 }
 
 void SerialDebugPrint(const char *text)
--- src/pin_definitions.h
+++ src/pin_definitions.h
@@ -3,33 +3,33 @@
 
 #define PIN_COUNT 18
 
-#define VP 36
-#define VN 39
-#define CLK 6
-#define D0 7
-#define D1 8
-#define D2 9
-#define D3 10
-#define TX 1
-#define RX 3
+#define VP   36
+#define VN   39
+#define CLK  6
+#define D0   7
+#define D1   8
+#define D2   9
+#define D3   10
+#define TX   1
+#define RX   3
 
-#define JOYSTICK_X 34
-#define JOYSTICK_Y 35
-#define DRIVE_SPEED VP
-#define BRUSH_SPEED VN
-#define BATTERY_VOLTAGE 32
-#define WATER_BUTTON 21
-#define LAMP_BUTTON 33
-#define START_BUTTON 25
-#define PASS_BUTTON 26
-#define BRUSH_FRONT_CW 27
-#define BRUSH_FRONT_CCW 14
-#define BRUSH_REAR_CW 12
-#define BRUSH_REAR_CCW 2
-#define EMERGENCY_STOP_BUTTON 16
-#define WARNING_LED_RED 17
-#define WARNING_LED_GREEN 5
-#define BATTERY_LED_RED 22
-#define BATTERY_LED_GREEN 23
+#define JOYSTICK_X              34
+#define JOYSTICK_Y              35
+#define DRIVE_SPEED             VP
+#define BRUSH_SPEED             VN
+#define BATTERY_VOLTAGE         32
+#define WATER_BUTTON            21
+#define LAMP_BUTTON             33
+#define START_BUTTON            25
+#define PASS_BUTTON             26
+#define BRUSH_FRONT_CW          27
+#define BRUSH_FRONT_CCW         14
+#define BRUSH_REAR_CW           12
+#define BRUSH_REAR_CCW          2
+#define EMERGENCY_STOP_BUTTON   16
+#define WARNING_LED_RED         17
+#define WARNING_LED_GREEN       5
+#define BATTERY_LED_RED         22
+#define BATTERY_LED_GREEN       23
 
 #endif /* PIN_DEFINITIONS_H */
